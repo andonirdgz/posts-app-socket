176db44ce094ded8c0379bb71b984b9fc3ebb9cf
andonirdgz <andoni_rdgz@outlook.com>
2021-07-24 13:20:22 -0500

    Removed console log when finish editing

--- frontend/src/pages/Feed/Feed.js
+++ frontend/src/pages/Feed/Feed.js
@@ -36,7 +36,9 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
+
     const socket = openSocket("http://localhost:8080");
+
     socket.on("posts", (data) => {
       if (data.action === "create") {
         this.addPost(data.post);
@@ -181,7 +183,6 @@ class Feed extends Component {
         return res.json();
       })
       .then((resData) => {
-        console.log(resData);
         // const post = {
         //   _id: resData.post._id,
         //   title: resData.post.title,
